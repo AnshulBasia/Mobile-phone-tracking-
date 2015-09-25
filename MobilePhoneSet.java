3727b9ad2b813d82c76cf1a47939b639ef113a71
Anshul Basia <ee3140502@iitd.ac.in>
2015-09-25 17:18:28 +0530

    Create MobilePhoneSet.java

---
+++ MobilePhoneSet.java
@@ -0,0 +1,17 @@
+
+public class MobilePhoneSet
+{  Myset s=new Myset();
+   MobilePhoneSet()
+   {Myset s=new Myset();}
+   
+   public void reg(MobilePhone a)
+   {
+     s.Insert(a);
+   }
+   public void dreg(MobilePhone a)
+   {
+     s.Delete(a);
+   }
+   
+  
+}
